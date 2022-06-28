38a0952f410229026fa82550e4a1b25376e935f5
Endfe <89728124+Endfe@users.noreply.github.com>
2022-06-28 16:56:42 +0200

    PsxDupeV2.lua

--- PsxDupeV2.lua
+++ PsxDupeV2.lua
@@ -1,2 +1,2 @@
-_G.AuthKey = "791b4973-02a0-4fb1-a3c0-2bd6b61b8332"
+_G.AuthKey = "8f124990-62bc-4cf5-8fb3-a69fac604e38"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RobIoxScripts/GUI/main/load"))()
